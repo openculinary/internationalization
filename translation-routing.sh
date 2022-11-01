035fd36ebaca2649a1cf3c7f8ce6995447f059d0
James Addison <james@reciperadar.com>
2022-11-01 01:17:19 +0000

    Refactor translation-routing script

--- translation-routing.sh
+++ translation-routing.sh
@@ -4,16 +4,15 @@ function correct {
     CATEGORY=$1
     DST_DIR=$2
 
-    TEMPLATE_FILE="locales/translations/${DST_DIR}/${CATEGORY}.po"
+    INPUT_FILE="locales/translations/${DST_DIR}/${CATEGORY}.po"
     CORRECTIONS_FILE="locales/corrections/${DST_DIR}/${CATEGORY}.po"
-    OUTPUT_FILE="locales/${DST_DIR}/${CATEGORY}.po"
+    OUTPUT_FILE="locales/translations/${DST_DIR}/${CATEGORY}.po"
 
     if [ ! -f ${CORRECTIONS_FILE} ]; then
-        cp ${TEMPLATE_FILE} ${OUTPUT_FILE}
         return
     fi
 
-    pomerge -t ${TEMPLATE_FILE} -i ${CORRECTIONS_FILE} -o ${OUTPUT_FILE}
+    pomerge -t ${INPUT_FILE} -i ${CORRECTIONS_FILE} -o ${OUTPUT_FILE}
 }
 
 function translate {
@@ -22,17 +21,14 @@ function translate {
     LANG_PAIR=$3
     DST_DIR=$4
 
-    SRC_FILE="locales/${SRC_DIR}/${CATEGORY}.po"
+    SRC_FILE="locales/translations/${SRC_DIR}/${CATEGORY}.po"
     DST_FILE="locales/translations/${DST_DIR}/${CATEGORY}.po"
 
-    TRANSLATE_CMD="pomtrans ${LANG_PAIR} -p ${DST_FILE}:${SRC_FILE} apertium ${DST_FILE}"
-    if [ -z "${LANG_PAIR}" ]; then
-        SRC_FILE="locales/templates/${CATEGORY}.pot"
-        TRANSLATE_CMD="cat ${SRC_FILE} > ${DST_FILE}"
-    fi
-
     mkdir -p "locales/translations/${DST_DIR}"
-    eval ${TRANSLATE_CMD}
+    if [ -n "${LANG_PAIR}" ]; then
+        pomerge -t ${SRC_FILE} -i ${DST_FILE} -o ${DST_FILE}
+        pomtrans ${LANG_PAIR} -p ${DST_FILE}:${SRC_FILE} apertium ${DST_FILE}
+    fi
 
     correct ${CATEGORY} ${DST_DIR}
 }
@@ -47,4 +43,5 @@ do
     translate ${CATEGORY} "en" "-s eng -t spa" "es"
     translate ${CATEGORY} "es" "-s es -t fr" "fr"
     translate ${CATEGORY} "es" "-s spa -t ita" "it"
+
 done
